7cd70e93a995b8d024b98c131adcf5edd99db9c7
CmdCoder585 <47717085+CmdCoder585@users.noreply.github.com>
2021-04-03 10:10:25 +0100

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Calculator
+{
+	public class Calculator
+	{
+		static void Main()
+		{
+			while (true)
+			{
+				Console.WriteLine("enter number");
+				int.TryParse(Console.ReadLine(), out int a);
+				Console.WriteLine("enter operation");
+				string op = Console.ReadLine();
+				Console.WriteLine("enter number");
+				int.TryParse(Console.ReadLine(), out int b);
+				switch (op)
+				{
+					case "+":
+						Console.WriteLine($"{a + b}");
+						break;
+					case "-":
+						Console.WriteLine($"{a - b}");
+						break;
+					case "*":
+						Console.WriteLine($"{a * b}");
+						break;
+					case "/":
+						Console.WriteLine($"{a / b}");
+						break;
+				}
+
+			}
+		}
+	}
+}
